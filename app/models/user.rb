fd8bf6d20e7e31d1cfa00230a928845216044173
yoshimori <2311140023p@ed.fuk.kindai.ac.jp>
2026-01-17 04:57:15 +0000

    デザイン修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,22 @@
 class User < ApplicationRecord
   has_secure_password
+
   has_many :shops, dependent: :nullify
+  
   validates :uid, presence: true, uniqueness: true
   
   has_many :likes, dependent: :destroy
+  
   has_many :liked_shops, through: :likes, source: :shop
-  
-  def already_liked?(shop)
-    self.likes.exists?(shop_id: shop.id)
-  end
-  
   has_many :liked_items, through: :likes, source: :item
   
-  def already_liked?(item)
-    self.likes.exists?(item_id: item.id)
+  def already_liked?(object)
+    if object.is_a?(Shop)
+      self.likes.exists?(shop_id: object.id)
+    
+    elsif object.is_a?(Item)
+      self.likes.exists?(item_id: object.id)
+    end
   end
+
 end
